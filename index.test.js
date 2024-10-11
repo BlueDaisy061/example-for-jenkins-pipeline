7c2ccdfea2cf6bdc8a4739d6e0c10e5632678950
Daisy Nguyen <nttduong.c3tqcap.a1@gmail.com>
2024-10-11 22:30:06 +0700

    feat: update test

--- index.test.js
+++ index.test.js
@@ -2,4 +2,8 @@ describe("Example unit test", () => {
   it("should return true", () => {
     expect(true).toBeTruthy();
   });
+
+  it("should return false", () => {
+    expect(false).toBeFalsy();
+  });
 });
